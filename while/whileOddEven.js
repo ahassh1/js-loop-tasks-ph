0847301173d61fe0fbd868eb25c54ab1af3a7351
ahassh10 <ahasanhabibpolash420@gmail.com>
2025-02-12 17:18:57 +0600

    while for odd and even numbers

--- while/whileOddEven.js
+++ while/whileOddEven.js
@@ -3,16 +3,22 @@
 Subtask-1:
 
 Find all the odd numbers from 61 to 100.
-
- */
+*/
+// let i=61;
+// while(i<=100){
+//     console.log(i)
+//     i+=2;
+// }
 
 
 /***
-
 Subtask-2:
 
 Find all the even numbers from 78 to 98.
-
  */
 
-/*programming hero*/
+let i=78;
+while(i<=98){
+    console.log(i)
+    i+=2;
+}
